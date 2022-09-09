8a54164919367fdb9c0647851f1c2b97d1745ae9
jbijlsma <jeroen_bijlsma@yahoo.com>
2022-09-09 12:48:48 +0800

    Get real iphone working with firebase emulators

--- lib/services/google_cloud_functions.dart
+++ lib/services/google_cloud_functions.dart
@@ -11,8 +11,9 @@ class GoogleCloudFunctions {
         region: AppConfig.instance.cloudFunctionsRegion);
 
     if (AppConfig.instance.useEmulator) {
+      print('Using emulator. Platform ${Platform.isIOS}');
       caller.useFunctionsEmulator(
-          Platform.isIOS ? 'localhost' : '10.0.2.2', 5001);
+          Platform.isIOS ? '0.0.0.0' : '10.0.2.2', 5001);
     }
 
     final callable = caller.httpsCallable(name);
