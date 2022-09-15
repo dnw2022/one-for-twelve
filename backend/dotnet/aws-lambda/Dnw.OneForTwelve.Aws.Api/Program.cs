5aaf54d85874270dd40339e72db6db7853a50088
jbijlsma <jeroen_bijlsma@yahoo.com>
2022-09-15 09:51:32 +0800

    Cleanup endpoints

--- backend/dotnet/aws-lambda/Dnw.OneForTwelve.Aws.Api/Program.cs
+++ backend/dotnet/aws-lambda/Dnw.OneForTwelve.Aws.Api/Program.cs
@@ -16,28 +16,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.Map("/", (IHttpContextAccessor ctx) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var architecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture;
+    var dotnetVersion = Environment.Version.ToString();
+    return $"Architecture: {architecture}, .NET Version: {dotnetVersion}, {ctx.HttpContext?.Request.Path}";
+});
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
