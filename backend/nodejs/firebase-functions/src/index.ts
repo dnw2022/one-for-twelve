2262d6bdc3896a2020b41ff9641ca2c79a54ce3e
jbijlsma <jeroen_bijlsma@yahoo.com>
2022-10-04 17:56:27 +0800

    Rewrite google cloud function or 2nd gen

--- backend/nodejs/firebase-functions/src/index.ts
+++ backend/nodejs/firebase-functions/src/index.ts
@@ -1,19 +1,17 @@
-import * as functions from "firebase-functions";
-import * as express from "express";
+import express from "express";
 import * as admin from "firebase-admin";
 
-import { GameCache } from "./game_cache";
-import { GameFactory } from "./game_factory";
+import { GameFactory, GameCache } from "@dnw-one-for-twelve/game-core";
 
-admin.initializeApp();
+admin.initializeApp({ projectId: "one-for-twelve-32778" });
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: any, res: any) => {
   res.status(200).send("Hello anonymous");
 });
 
-app.get("/games/:languageCode/:strategy", async (req, res) => {
+app.get("/games/:languageCode/:strategy", async (req: any, res: any) => {
   const { languageCode, strategy } = req.params;
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
@@ -43,7 +41,7 @@ app.get("/games/:languageCode/:strategy", async (req, res) => {
 });
 
 const initCache = async (useUnrevised: Boolean) => {
-  const resourcesPath = "./lib";
+  const resourcesPath = __dirname;
   const wordFiles = [`${resourcesPath}/words.csv`];
   const questionFiles = [`${resourcesPath}/questions.csv`];
 
@@ -58,4 +56,4 @@ const initCache = async (useUnrevised: Boolean) => {
 //tslint:disable-next-line no-floating-promises
 initCache(false);
 
-exports.gameApi = functions.https.onRequest(app);
+exports.gameApi = app;
