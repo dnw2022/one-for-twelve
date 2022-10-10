55303072d18a1fed63f7425690c17d6fb613498d
jbijlsma <jeroen_bijlsma@yahoo.com>
2022-10-10 08:13:23 +0800

    Validate token in aws sam using firebase-admin

--- backend/nodejs/aws-sam/src/index.ts
+++ backend/nodejs/aws-sam/src/index.ts
@@ -1,16 +1,31 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 import middy from "@middy/core";
+import admin from "firebase-admin";
 
 import { GameFactory, GameCache } from "@dnw-one-for-twelve/game-core";
+
+admin.initializeApp({ projectId: "one-for-twelve-32778" });
 
 export const lambdaHandler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
   const { languageCode, strategy } = event.pathParameters!;
-  const token = event.headers["Authorization"]?.replace("Bearer ", "");
+  const token = event.headers["authorization"]?.replace("Bearer ", "");
 
+  console.log(JSON.stringify(event.headers));
   console.log(token);
+
+  if (!token) {
+    return {
+      statusCode: 401,
+      body: "Bearer token missing from Authorization header",
+    };
+  }
+
+  const decodedToken = await admin.auth().verifyIdToken(token);
+
+  console.log(decodedToken);
 
   await initCache();
 
